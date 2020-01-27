63ad2f684ede6aac8dabc728b1e93c33ed94b578
Baptiste Dumont <02300bd@gmail.com>
2020-01-27 15:59:07 +0100

    logged in

--- Epicture/app/ViewImages.js
+++ Epicture/app/ViewImages.js
@@ -8,10 +8,12 @@ import React, {
   Image,
   Dimensions,
   ListView
-} from 'react-native';
-let windowWidth = Dimensions.get('window').width;
-import API from './api';
-let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+} from 'react-native'
+
+let windowWidth = Dimensions.get('window').width
+import API from './api'
+let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+
 class ViewImages extends Component {
   constructor (props) {
     super(props)
@@ -21,54 +23,75 @@ class ViewImages extends Component {
       images: []
     }
   }
-componentDidMount () {
+
+  componentDidMount () {
     API.get(this.props.category)
-      .then((response) => {
-        this.setState({
-          dataSource: ds.cloneWithRows(response.data.items),
-          loading: false
+        .then((response) => {
+          this.setState({ dataSource: ds.cloneWithRows(response.data.items), loading: false })
+        }, (error) => {
+          console.log('error: ', error)
         })
-      }, (error) => {
-      console.log('error: ', error)
-    })
   }
+
   renderRow (rowData) {
     if (rowData.link.match(/\.(jpg|png|gif)/g)) {
       return (
-        <View>
-          <Image
-            source={{ uri: rowData.link }}
-            style={{width: windowWidth, height: windowWidth}} />
-        </View>)
+          <View>
+            <Image source={{ uri: rowData.link }} style={{width: windowWidth, height: windowWidth}} />
+          </View>)
     } else {
       return null
     }
   }
+
   render () {
     let { loading, images } = this.state
+
     if (loading) {
       images = (
-        <View style={style.loadingContainer}>
-          <Text style={style.loading}>Loading images…</Text>
-        </View>)
+          <View style={style.loadingContainer}>
+            <Text style={style.loading}>Loading images...</Text>
+          </View>)
     }
+
     if (!loading) {
-      images = <ListView
-                 dataSource={this.state.dataSource}
-                 renderRow={this.renderRow.bind(this)} />
+      images = <ListView dataSource={this.state.dataSource} renderRow={this.renderRow.bind(this)} />
     }
+
     return (
-      <View style={{flex: 1}}>
-        <TouchableHighlight
-          underlayColor='transparent'
-          onPress={this.props.closeModal.bind(this)}
-          style={style.closeButton}>
-          <Text style={style.closeButtonText}>CLOSE</Text>
-        </TouchableHighlight>
-        <ScrollView style={{flex: 1}}>
-          {images}
-        </ScrollView>
-      </View>
+        <View style={{flex: 1}}>
+          <TouchableHighlight underlayColor='transparent' onPress={this.props.closeModal.bind(this)} style={style.closeButton}>
+            <Text style={style.closeButtonText}>CLOSE</Text>
+          </TouchableHighlight>
+          <ScrollView style={{flex: 1}}>
+            {images}
+          </ScrollView>
+        </View>
     )
   }
 }
+
+const style = StyleSheet.create({
+  closeButton: {
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomColor: '#e6e6e6',
+    borderBottomWidth: 1
+  },
+  closeButtonText: {
+    fontSize: 16,
+    color: '#afafaf',
+    marginTop: 10
+  },
+  loadingContainer: {
+    alignItems: 'center',
+    marginTop: 160
+  },
+  loading: {
+    fontSize: 22,
+    color: '#7f7f7f'
+  }
+})
+
+export default ViewImages
--- Epicture/index.js
+++ Epicture/index.js
@@ -1,13 +1,165 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import { AppRegistry, StyleSheet, Text, View, Button, Linking, AsyncStorage, Image } from "react-native";
+
+import { authorize } from 'react-native-app-auth';
+
 
 class App extends Component {
+
+    state = { logged: false };
+
+    componentDidMount() {
+
+
+        Linking.getInitialURL().then((url) => {
+            let username;
+            let userId;
+            let token;
+
+            if (url) {
+                console.log('Initial url is: ' + url);
+                let regex = /([^=#]+)=([^&#]*)/g,
+                    params = {},
+                    match
+                while ((match = regex.exec(url))) {
+                    params[match[1]] = match[2]
+                    switch (match[1]) {
+                        case "&account_username":
+                            username = match[2];
+                            break;
+                        case "&account_id":
+                            userId = match[2];
+                            break;
+                        case "access_token":
+                            token = match[2];
+                            break;
+
+                    }
+
+
+                    // if(match[1] == "access_token") {
+                    //     this.saveItem('userToken', match[2]).then(r => {
+                    //         console.log("done");
+                    //     });
+                    // }
+                        console.log(match[1], match[2])
+                }
+                this.saveItem('userToken', {
+                    'userId' : userId,
+                    'username': username,
+                    'token': token,
+                }).then(r => {
+                    this.setState(previousState => (
+                        { logged: true }
+                    ));
+                    console.log("done");
+                });
+                console.log(username);
+            }
+        }).catch(err => console.error('An error occurred', err));
+    }
+
+    async saveItem(item, selectedValue) {
+        try {
+            await AsyncStorage.setItem('token', selectedValue.token);
+            await AsyncStorage.setItem('userid', selectedValue.userId);
+            await AsyncStorage.setItem('username', selectedValue.username);
+        } catch (error) {
+            console.error('AsyncStorage error: ' + error.message);
+        }
+    }
+
+    async getToken() {
+        let userToken;
+        await AsyncStorage.getItem('token').then((token) => {
+            userToken = token;
+        });
+        return userToken;
+    }
+
+    async getUserId() {
+        let userId;
+        await AsyncStorage.getItem('userid').then((token) => {
+            userId = token;
+        });
+        return userId;
+    }
+
+    async getUserName() {
+        let userName;
+        await AsyncStorage.getItem('username').then((token) => {
+            userName = token;
+        });
+        // console.log(userToken);
+        return userName;
+    }
+
+    async getUserInfo()
+    {
+        let username = await this.getUserName();
+        let token =  await this.getToken();
+
+        let userInfo;
+
+        console.log(username);
+        console.log(token);
+
+        fetch('https://api.imgur.com/3/account/' + username, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+        }).then((response) => response.json())
+            .then((responseJson) => {
+                userInfo = responseJson;
+                // console.log(userInfo);
+                return userInfo;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    async disconnect() {
+        await AsyncStorage.clear();
+        this.setState(previousState => (
+            { logged: false }
+        ));
+    }
+
     render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome</Text>
-            </View>
-        );
+
+        if (this.state.logged) {
+            let userInfo = this.getUserInfo();
+            console.log('user info : ', userInfo);
+            return (
+                <View style={styles.container}>
+                    {/*<Image*/}
+                    {/*    style={{width: 50, height: 50}}*/}
+                    {/*    source={this.getUserInfo().data.avatar}*/}
+                    {/*/>*/}
+                    <Text style={styles.welcome}>Welcome to React Native!</Text>
+                    <Button title="Get my token" onPress={() => {
+                        this.getToken()
+                    }}/>
+                    {/*<Button title="Get user info" onPress={ ()=>{ this.getUserInfo()}} />*/}
+                    <Button title="Se déconnecter" onPress={() => {
+                        this.disconnect()
+                    }}/>
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.welcome}>Welcome to React Native!</Text>
+                    <Button title="Login" onPress={() => {
+                        Linking.openURL('https://api.imgur.com/oauth2/authorize?client_id=52cbd80405a2505&response_type=token')
+                    }}/>
+                </View>
+            );
+        }
     }
 }
 
