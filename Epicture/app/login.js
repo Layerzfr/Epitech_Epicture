1098b6da633ece944ddb95f2944ba4cc777e8979
Radatovic <radatovic.damien@gmail.com>
2020-01-30 16:14:10 +0100

    login-page design update

--- Epicture/app/login.js
+++ Epicture/app/login.js
@@ -1,7 +1,7 @@
 import React, {
     Component,
 } from 'react';
-import {Button, Linking, Text, View} from "react-native";
+import {Button, ImageBackground, Linking, Text, TouchableOpacity, View, StyleSheet} from "react-native";
 
 class Login extends Component{
     state = {};
@@ -12,13 +12,53 @@ class Login extends Component{
 
     render() {
         return (<View>
-            <Button title="Se connecter" onPress={() => {
-                Linking.openURL('https://api.imgur.com/oauth2/authorize?client_id=52cbd80405a2505&response_type=token')
-            }}/>
-            <Text>
+            <ImageBackground source={require('../Assert/PNG/Fond_Logo.png')}
+                             style={{width: '100%', height: '100%'}}>
+                <TouchableOpacity
+                    style={styles.button} onPress={() => {
+                    Linking.openURL('https://api.imgur.com/oauth2/authorize?client_id=52cbd80405a2505&response_type=token')
+                }}
+                >
+                    <Text style={styles.text}>Get Started</Text>
+                </TouchableOpacity>
+                <View style={styles.buttonShadow}></View>
+            </ImageBackground>
+            {/*<Text>
                 Aides
-            </Text>
+            </Text>*/}
         </View>);
     }
 }
+const styles = StyleSheet.create({
+    text: {
+        color: '#025E73',
+        fontSize: 20,
+        textAlign: 'center',
+
+    },
+    button:{
+        zIndex: 1,
+        display: 'flex',
+        width: '50%',
+        height: 45,
+        borderRadius: 25,
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginTop: 350,
+    },
+    buttonShadow:{
+        zIndex: 0,
+        display: 'flex',
+        width: '51%',
+        height: 51,
+        borderRadius: 25,
+        backgroundColor: '#262626',
+        alignSelf: 'center',
+        bottom: 47,
+        opacity: 0.15,
+    },
+})
+
 export default Login
+
